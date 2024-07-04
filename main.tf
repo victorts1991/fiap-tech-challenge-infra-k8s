f1346246bbdc82c677274552ae3ba123a5e2d442
Victor Toupitzen Specian <victor.specian@kobel.com.br>
2024-07-04 04:38:10 -0300

    add sqs for terraform

--- main.tf
+++ main.tf
@@ -150,3 +150,38 @@ module "eks" {
     }
   }
 }
+
+resource "aws_sqs_queue" "sh_queue" {
+  name                       = "sh-fiap-tech-challenge-5-queue"
+  delay_seconds              = 10
+  visibility_timeout_seconds = 30
+  max_message_size           = 2048
+  message_retention_seconds  = 86400
+  receive_wait_time_seconds  = 2
+  sqs_managed_sse_enabled = true
+}
+
+data "aws_iam_policy_document" "sh_sqs_policy" {
+  statement {
+    sid    = "shsqsstatement"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "sqs:SendMessage",
+      "sqs:ReceiveMessage"
+    ]
+    resources = [
+      aws_sqs_queue.sh_queue.arn
+    ]
+  }
+}
+
+resource "aws_sqs_queue_policy" "sh_sqs_policy" {
+  queue_url = aws_sqs_queue.sh_queue.id
+  policy    = data.aws_iam_policy_document.sh_sqs_policy.json
+}
